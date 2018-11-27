1433e1966b2d2aabf6b63012610b5e5cd346b954
Brady <brady.cunningham@ryerson.ca>
2018-11-27 18:03:07 -0500

    Initial Android Studio commit

---
+++ app/src/main/java/com/cps731/group11/splits/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.cps731.group11.splits;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    // Initial Android Studio commit
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
