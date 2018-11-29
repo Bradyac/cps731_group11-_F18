26b0bb6a1074695004a97ec2354e00f6a43b220d
Brady <brady.cunningham@ryerson.ca>
2018-11-29 01:26:53 -0500

    Configure Home Tab tabLayout fragments with ViewPager

--- app/src/main/java/com/cps731/group11/splits/HomeFragment.java
+++ app/src/main/java/com/cps731/group11/splits/HomeFragment.java
@@ -3,16 +3,76 @@ package com.cps731.group11.splits;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+    private ViewPager viewPager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        //sectionsPageAdapter = new SectionsPageAdapter(getChildFragmentManager());
+        viewPager = view.findViewById(R.id.homeViewPager);
+        setupViewPager(viewPager);
+        TabLayout tabLayout = view.findViewById(R.id.home_tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return view;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getChildFragmentManager());
+        adapter.addFragment(new BalanceHomeFragment(),"Balance");
+        adapter.addFragment(new LentHomeFragment(),"Lent");
+        adapter.addFragment(new BorrowedHomeFragment(),"Borrowed");
+        viewPager.setAdapter(adapter);
+
+    }
+
+    static class SectionsPageAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+
+        public SectionsPageAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
     }
 }
