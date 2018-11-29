49acf8f4bbdf148f07feca4993f5c9b3578d266f
Lahiru <cheesaru@gmail.com>
2018-11-28 20:12:34 -0500

    pulled from master

--- app/src/main/java/com/cps731/group11/splits/model/Friend.java
+++ app/src/main/java/com/cps731/group11/splits/model/Friend.java
@@ -1,7 +1,15 @@
 package com.cps731.group11.splits.model;
 
+import java.sql.Blob;
+
 public class Friend {
 
-    public Friend()
+    private Blob picture;
+    private String name;
+
+    public Friend(Blob picture, String name){
+        this.picture = picture;
+        this.name = name;
+    }
 
 }
