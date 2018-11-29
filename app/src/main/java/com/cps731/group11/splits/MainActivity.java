1c75c1b216ef44353e281767870676c5bacb86ba
Brady <brady.cunningham@ryerson.ca>
2018-11-28 20:04:22 -0500

    Create login page issue #1

--- app/src/main/java/com/cps731/group11/splits/MainActivity.java
+++ app/src/main/java/com/cps731/group11/splits/MainActivity.java
@@ -5,9 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
