a131c25fac2114c6e980bc0706079799954cc588
Lahiru <cheesaru@gmail.com>
2018-11-30 02:12:54 -0500

    added the recyclerview adapter to the fragment with dummy data

--- app/src/main/java/com/cps731/group11/splits/FriendsFragment.java
+++ app/src/main/java/com/cps731/group11/splits/FriendsFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,11 +20,25 @@ public class FriendsFragment extends Fragment {
 
     private LinearLayoutManager layoutManager;
     private FriendsAdapter friendsAdapter;
+    Fragment fragment;
     //
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.friendList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        Friend[] temp = {
+                new Friend("buddy")
+        };
+
+        FriendsAdapter friendsAdapter = new FriendsAdapter(temp);
+        recyclerView.setAdapter(friendsAdapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        return rootView;
 
     }
 
