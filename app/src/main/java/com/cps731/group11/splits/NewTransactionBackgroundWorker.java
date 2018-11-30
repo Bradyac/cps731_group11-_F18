12f51b53a5733d44d749045b21665ddbdec7d7a6
Jacky Tran <jackytran98@gmail.com>
2018-11-29 22:53:27 -0500

    createTransaction finished

--- app/src/main/java/com/cps731/group11/splits/NewTransactionBackgroundWorker.java
+++ app/src/main/java/com/cps731/group11/splits/NewTransactionBackgroundWorker.java
@@ -17,31 +17,96 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
 public class NewTransactionBackgroundWorker extends AsyncTask<String, String, String>{
-    Context context;
-    NewTransactionBackgroundWorker (Context ctx){
-        context = ctx;
+    Fragment fragment;
+    ArrayList<String> friendsList;
+
+    NewTransactionBackgroundWorker (Fragment input){
+        fragment = input;
     }
+
+    protected void onPreExecute(){
+        super.onPreExecute();
+        friendsList = new ArrayList<>();
+    }
+
     @Override
-    protected String doInBackground(String... params){
+    protected String doInBackground(String... params) {
         String type = params[0];
-        String nt_url = "http://splits.atwebpages.com/newTransaction.php";
-        if(type.equals("newTransaction")){
-            try{
-                URL url = new URL(nt_url);
-                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+        System.out.println(type);
+        if (type.equalsIgnoreCase("Friends")) {
+            String id = params[1];
+            try {
+                URL url = new URL("http://splits.atwebpages.com/getFriends.php");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.setRequestMethod("POST");
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setDoInput(true);
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
 
-            }
-            catch (MalformedURLException e){
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String server_reply = "";
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    server_reply += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                System.out.println(server_reply);
+                return server_reply;
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            catch (IOException e){
+        } else if (type.equalsIgnoreCase("Transaction")) {
+            String id = params[1];
+            String amount = params[2];
+            String description = params[3];
+            String friend = params[4];
+            String checkedRadio = params[5];
+            String login_url = "http://splits.atwebpages.com/newTransaction.php";
+            try {
+                // Connection setup
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+
+                // Request to server
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String client_request = URLEncoder.encode("user", "UTF-8") + "=" + URLEncoder.encode(id, "UTF-8") + "&"
+                        + URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode(checkedRadio, "UTF-8")
+                        + URLEncoder.encode("friend", "UTF-8") + "=" + URLEncoder.encode(friend, "UTF-8")
+                        + URLEncoder.encode("amount", "UTF-8") + "=" + URLEncoder.encode(amount, "UTF-8")
+                        + URLEncoder.encode("description", "UTF-8") + "=" + URLEncoder.encode(description, "UTF-8");
+                bufferedWriter.write(client_request);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+
+                // Reply from server
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String server_reply = "";
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    server_reply += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+
+                // Close connection
+                httpURLConnection.disconnect();
+                return server_reply;
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
--- app/src/main/java/com/cps731/group11/splits/NewTransactionFragment.java
+++ app/src/main/java/com/cps731/group11/splits/NewTransactionFragment.java
@@ -7,15 +7,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class NewTransactionFragment extends Fragment {
+    EditText nt_Amount, nt_Description;
+    Spinner nt_Friends;
+    RadioGroup transactionTypes;
+    String userID;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> listItems = new ArrayList<>();
+    Fragment fragment;
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_new_transaction, container, false);
+        fragment = this;
+        userID = ((MainActivity)getActivity()).getCurrentUser();
+        NewTransactionBackgroundWorker newTransactionBackgroundWorker = new NewTransactionBackgroundWorker(fragment);
+        newTransactionBackgroundWorker.execute("Friends", userID);
+        //nt_Amount = getView().findViewById(R.id.et_ntAmount);
+        //nt_Description = getView().findViewById(R.id.et_ntDescription);
+        //nt_Friends = getView().findViewById(R.id.spin_ntFriends);
+        //adapter = new ArrayAdapter<String>(getContext(), R.layout.fragment_new_transaction,R.id.spin_ntFriends,listItems);
+        //nt_Friends.setAdapter(adapter);
+        //transactionTypes = getView().findViewById(R.id.radio_transactionTypes);
 
-        String userID = ((MainActivity)getActivity()).getCurrentUser();
+        View view = inflater.inflate(R.layout.fragment_new_transaction, container, false);
         return view;
+    }
+    public void createTransaction(View view){
+        String str_ntAmount = nt_Amount.getText().toString();
+        String str_ntDescription = nt_Description.getText().toString();
+        String str_ntFriends = nt_Friends.getSelectedItem().toString();
+
+        int checkedRadioID = transactionTypes.getCheckedRadioButtonId();
+        RadioButton checkedRadio = (RadioButton)transactionTypes.findViewById(checkedRadioID);
+        String str_ntRadio = (String)checkedRadio.getText();
+
+        NewTransactionBackgroundWorker newTransactionBackgroundWorker = new NewTransactionBackgroundWorker(fragment);
+        newTransactionBackgroundWorker.execute("Transaction",userID,str_ntAmount,str_ntDescription,str_ntFriends,str_ntRadio);
     }
 }
