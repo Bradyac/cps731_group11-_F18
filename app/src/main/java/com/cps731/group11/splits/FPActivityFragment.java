43ce440953c3f788e0d5e6d9e69925b26d1a4284
JIMMY <jimmy2301@gmail.com>
2018-11-30 05:32:45 -0500

    nearing completion for the activities and history tab, ironing out pro

--- app/src/main/java/com/cps731/group11/splits/FPActivityFragment.java
+++ app/src/main/java/com/cps731/group11/splits/FPActivityFragment.java
@@ -27,28 +27,17 @@ public class FPActivityFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        listViewItems = new ArrayList<String>();
-        lv = (ListView) getActivity().findViewById(R.id.activity_activities_dynamic);
 
 
-
-
-
-        for (int i =0;i<transArr.size();i++){
-            String listViewItem = "You " + typeArr.get(i) + " " + amtArr.get(i) + " with " + user2Arr.get(i) + " for " + descArr.get(i) + " on " + dateArr.get(i) + ".";
-            listViewItems.add(listViewItem);
-}
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_list_item_1, listViewItems);
-
-        lv.setAdapter(arrayAdapter);
     }
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_activity_history, container, false);
+        return inflater.inflate(R.layout.fragment_activity_activities, container, false);
     }
+    /*
     public void transferData(ArrayList<String> transArrq,ArrayList<String>  user1Arrq,ArrayList<String>  user2Arrq,ArrayList<String>  typeArrq,ArrayList<String>  descArrq,ArrayList<String>  amtArrq,ArrayList<String>  dateArrq) {
         // Logic to manipulate transferred data
         transArr = transArrq;
@@ -58,6 +47,6 @@ public class FPActivityFragment extends Fragment {
         descArr =descArrq;
         amtArr =amtArrq;
         dateArr =dateArrq;
-    }
+    }*/
 }
-    //fragment_activity.xml
+
